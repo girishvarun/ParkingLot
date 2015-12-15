82a4688e3b7f5199189e641c4cb2c3ee06696909
girishvarun <girishvarun@yahoo.com>
2015-12-15 23:48:31 +0530

    Revert "adding project"
    This reverts commit 9af685d86ccd87ab45468c48b6ee10de7a1917b5.

--- src/com/model/CarNotParkedException.java
+++
@@ -1,7 +0,0 @@
-package com.model;
-
-/**
- * Created by girishv on 12/12/15.
- */
-public class CarNotParkedException extends Exception {
-}
--- src/com/model/ParkingSlotNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package com.model;
-
-/**
- * Created by girishv on 15/12/15.
- */
-public class ParkingSlotNotAvailableException extends Exception {
-}
